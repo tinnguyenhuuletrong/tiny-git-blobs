5daa44bfadcbb1fcfe69cb6f3fffaecfc2ca7729
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2025-05-28 17:21:35 +0700

    Add help command and enhance CLI output formatting in main.ts. Introduce printHelp function to display available commands and improve user guidance. Update command handling for 'add' and 'getBlob' to provide clearer usage messages and error handling.

--- src/cli/src/main.ts
+++ src/cli/src/main.ts
@@ -31,16 +31,38 @@ function parseOptions() {
   };
 }
 
+function printHelp() {
+  console.log("\x1b[37mAvailable commands:\x1b[0m");
+  console.log(
+    "\x1b[37m\thead: Fetch and display the current head commit.\x1b[0m"
+  );
+  console.log(
+    "\x1b[37m\tadd <fileName> <fileContent>: Add a new file with the specified content.\x1b[0m"
+  );
+  console.log(
+    "\x1b[37m\tgetBlob <blobHash>: Retrieve and display the content of a blob by its hash.\x1b[0m"
+  );
+  console.log("\x1b[37m\thelp: Display this help message.\x1b[0m");
+}
+
 async function main() {
   const appOptions = parseOptions();
   const storage = new FileSystemStorageAdapter(appOptions.storagePath);
 
   console.log("CLI app");
-  console.log("\t-storagePath:", resolve(appOptions.storagePath));
+  console.log(
+    "\x1b[34m\t-storagePath:\x1b[0m",
+    resolve(appOptions.storagePath)
+  );
+  printHelp();
 
   for await (const line of console) {
     const [cmd, ...args] = line.split(" ");
     switch (cmd) {
+      case "help": {
+        printHelp();
+        break;
+      }
       case "head": {
         const head = await fetchHead(storage);
         console.dir(head, { depth: 10 });
@@ -50,7 +72,7 @@ async function main() {
         const fileName = args[0];
         const fileContent = args.slice(1).join(" ");
         if (!(fileName && fileContent)) {
-          console.warn("usage: add <fileName> <fileContent>");
+          console.warn("\x1b[37musage: add <fileName> <fileContent>\x1b[0m");
           break;
         }
         const commit = await addFile(storage, fileName, fileContent);
@@ -59,8 +81,27 @@ async function main() {
 
         break;
       }
+      case "getBlob": {
+        const blobHash = args[0];
+        const hasObject = await storage.hasObject(blobHash);
+        if (!hasObject) {
+          console.error(`Blob with hash ${blobHash} not found`);
+          break;
+        }
+
+        const blobObj = await storage.getBlob(blobHash);
+        if (!blobObj) {
+          console.error(`Blob with hash ${blobHash} not found`);
+          break;
+        }
+
+        const decoder = new TextDecoder();
+        console.log(decoder.decode(blobObj.content));
+        break;
+      }
       default:
         console.log("unknown command", cmd);
+        printHelp();
         break;
     }
   }
